f924bd3ed839a56077d6dced6e6839bacea389e0
Nova-Prime <rawkstar.raju@gmail.com>
2017-05-24 08:31:11 +0530

    changed validte Function

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const express =  require('express');
 const test = require('./login-registration/send-otp.js');
 
 
-console.log(test.validate('90909090l'));
+console.log(test.validate('123456789kfksdf'));
