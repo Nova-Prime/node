732d1b9aef90da654b87f484694a15f8a7114579
Nova-Prime <rawkstar.raju@gmail.com>
2017-07-03 01:13:34 +0530

    added few modules

--- _init/root/app.js
+++ _init/root/app.js
@@ -4,17 +4,28 @@
     var dependencies = [
         'ui.router',
         'root.controllers',
+        'ngAnimate'
     ];
 
     var app = angular.module('root', dependencies);
 
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
+    app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise('/err');
         $stateProvider
             .state('/', {
                 url: '/',
                 templateUrl: '/root/core/home/home.html',
-                controller:'_home'
+                controller: '_home'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/root/core/login/login.html',
+                controller: '_login'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: '/root/core/contactUs/contact.html'
             })
 
     })
---
+++ _init/root/core/login/_login.js
@@ -0,0 +1,4 @@
+angular.module('root.controllers', [])
+    .controller('_login', function () {
+        console.log('Login Loaded');
+    });
